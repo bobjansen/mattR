4a55e53e4f36067cfc497887e1e67d5dbdbcd1ed
Bob Jansen <bobjansen@gmail.com>
2016-10-15 12:06:58 +0200

    Rename config to configure and add helper

---
+++ R/configure.R
@@ -0,0 +1,34 @@
+#' App configuration file
+#'
+#' @return the configuration
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' configure()
+#' }
+configure <- function() {
+  list(
+    port = 8080,
+    debug = FALSE
+  )
+}
+
+#' Get a value from the config or return the default
+#'
+#' @param config The configuration list
+#' @param key The key to look for
+#' @param default The default
+#'
+#' @return The key if found or the default
+#' @export
+#'
+#' @examples
+#' getConfigOrDefault(configure, port, 8888)
+getConfigOrDefault <- function(config, key, default) {
+  if (key %in% names(config)) {
+    config[[key]]
+  } else {
+    default
+  }
+}
