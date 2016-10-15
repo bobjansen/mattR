a5b9955c165a7dba885fb673a70a675ee31b8eed
Bob Jansen <bobjansen@gmail.com>
2016-10-15 10:17:09 +0200

    Set up a foo server

---
+++ R/server.R
@@ -0,0 +1,51 @@
+#' Build a mattR app object for serving by httpuv
+#'
+#' @return The app object
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' buildApp()
+#' }
+buildApp <- function() {
+  app <- list(
+    call = function(req) {
+      list(
+        status = 200L,
+        headers = list(
+          'Content-Type' = 'text/html'
+        ),
+        body = paste(
+          sep = "\r\n",
+          "foo"
+        )
+      )
+    },
+    onWSOpen = function(ws) {
+      ws$onMessage(function(binary, message) {
+        ws$send(message)
+      })
+    }
+  )
+
+  app
+}
+
+#' Run a server for testing mattR apps
+#'
+#' @return The httpuv handle to the server process
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' runTestServer()
+#' }
+runTestServer <- function() {
+  host <- "0.0.0.0"
+  port <- sample(1025:(2^16 - 1), 1)
+
+  app <- buildApp()
+
+  print(paste("Starting app on:", paste0(host, ":", port)))
+  httpuv::runServer(host, port, app)
+}
