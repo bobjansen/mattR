fe1263d638e48d1b024100a590ac2fa3567ec34d
Bob Jansen <bobjansen@gmail.com>
2017-01-22 14:33:06 +0100

    Setup managing the app

---
+++ R/manage.R
@@ -0,0 +1,46 @@
+#' Create a skeleton for the app
+#' @param path Target path for skeleton creation
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' buildApp()
+#' }
+skeleton <- function(path = getwd()) {
+  if (length(list.files(path)) +
+      length(list.dirs(path, recursive = FALSE)) > 0
+  ) {
+    cat("The directory is not empty, exiting.")
+    return()
+  } else {
+    fromPath <- file.path(system.file(package = "mattR"), "skeleton")
+    file.copy(file.path(fromPath, "routes.R"), path)
+    file.copy(file.path(fromPath, "manage"), path)
+  }
+
+  invisible(NULL)
+}
+
+#' Display the help message
+#' @param ... Ignored, just show the default help message
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' helpMessage()
+#' }
+helpMessage <- function(...) {
+    cat("Help message\n")
+}
+
+#' Display the usage message
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' usageMessage()
+#' }
+usageMessage <- function() {
+    cat("Usage message\n")
+}
+
