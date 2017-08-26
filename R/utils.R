30639e3354351367c9d077189694887f03c1c452
Bob Jansen <bobjansen@gmail.com>
2017-08-26 13:33:23 +0200

    Add fileUrl utility function

---
+++ R/utils.R
@@ -0,0 +1,21 @@
+#' fileUrl
+#' Return a URL for a file to be sent to the client. the file will be base64
+#' encoded and embedded in the URL.
+#'
+#' Taken from
+#' https://github.com/rstudio/shiny/blob/e30fac02eddbb7cd510d448e385c559c82009a24/R/shiny.R
+#'
+#' @param file The file name
+#' @param contentType The content type
+#' @export
+fileUrl <- function(file, contentType='application/octet-stream') {
+
+  bytes <- file.info(file)$size
+  if (is.na(bytes)) {
+    return()
+  }
+
+  fileData <- readBin(file, 'raw', n = bytes)
+  b64 <- httpuv::rawToBase64(fileData)
+  paste0('data:', contentType, ';base64,', b64)
+}
