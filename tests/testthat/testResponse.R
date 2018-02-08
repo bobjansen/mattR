d4434c7d962fa4f544d507894aa59639289d56ec
Bob Jansen <bobjansen@gmail.com>
2018-02-08 20:19:41 +0100

    Add test for the error response

--- tests/testthat/testResponse.R
+++ tests/testthat/testResponse.R
@@ -15,5 +15,19 @@ test_that("Setting up a response works", {
   expect_true("_middlewares_index" %in% names(resp))
 })
 
+test_that("Error responses are created correctly", {
+  errResp <- errorResponse()
+  expect_true(all(c("status", "body", "headers") %in% names(errResp)))
+  expect_true(errResp[["status"]] >= 500 && errResp[["status"]] < 600)
+  expect_true(grepl("500 Internal server error", errResp[["body"]]))
+  expect_true("Content-Type" %in% names(errResp[["headers"]]))
+  expect_equal(errResp[["headers"]][["Content-Type"]], "text/plain")
 
+  errResp2 <- errorResponse("Custom Message")
+  expect_true(all(c("status", "body", "headers") %in% names(errResp2)))
+  expect_true(errResp[["status"]] >= 500 && errResp2[["status"]] < 600)
+  expect_true(grepl("Custom Message", errResp2[["body"]]))
+  expect_true("Content-Type" %in% names(errResp[["headers"]]))
+  expect_equal(errResp[["headers"]][["Content-Type"]], "text/plain")
+})
 
